24ff9e74bce1fe1d90adc0aa52c3911241d9b415
Ondřej Hájek <ondrej.hajek.profi@gmail.com>
2025-05-26 19:27:35 +0200

    stuff

--- resources/views/auth/components/register-form.blade.php
+++ resources/views/auth/components/register-form.blade.php
@@ -66,10 +66,10 @@
 
             <div>
                 <div class="flex items-center justify-between">
-                    <label for="password_confirmed" class="block text-sm/6 font-medium text-gray-900">Confirm password</label>
+                    <label for="password_confirmation" class="block text-sm/6 font-medium text-gray-900">Confirm password</label>
                 </div>
                 <div class="mt-2">
-                    <input type="password" name="password_confirmed" id="password_confirmed" autocomplete="current-password" required
+                    <input type="password" name="password_confirmation" id="password_confirmation" autocomplete="current-password" required
                         class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                 </div>
             </div>
--- resources/views/components/list-button.blade.php
+++ resources/views/components/list-button.blade.php
@@ -1,7 +1,5 @@
 <li>
-    <div class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-      <button type="submit" {{ $attributes->class(['ms-3']) }}>
-          {{ $slot }}
-      </button>
-    </div>
+  <button type="submit" {{ $attributes->class(['ms-3 w-full text-start cursor-pointer']) }}>
+    {{ $slot }}
+  </button>
 </li>
