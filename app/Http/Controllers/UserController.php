8b2efbd35730318302e534adae9c7d95342beca7
Ondřej Hájek <ondrej.hajek.profi@gmail.com>
2025-05-27 20:03:15 +0200

    user stuff

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index() {
-        return view('users.index');
+        $users = User::paginate(10);
+        return view('users.index', ['users' => $users]);
     }
     public function create() {
         return view('users.create');
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,3 +1,52 @@
+@php
+use App\Helpers\DateFormatter;
+@endphp
+
 <x-sidebar>
-    <h1>User list</h1>
+    <div class="flex-1 flex flex-col gap-2">
+        <x-table dataLength="{{ count($users) }}">
+            <x-table-head>
+                <x-table-cell header>
+                    Name
+                </x-table-cell>
+                <x-table-cell header>
+                    Email
+                </x-table-cell>
+                <x-table-cell header>
+                    Birth date
+                </x-table-cell>
+                <x-table-cell header>
+                    Role
+                </x-table-cell>
+                <x-table-cell header>
+                    Created
+                </x-table-cell>
+                <x-table-cell header />
+            </x-table-head>
+            <x-table-body>
+                @foreach ($users as $user)
+                    <x-table-row>
+                        <x-table-cell>
+                            {{ $user->first_name . ' ' . $user->last_name }}
+                        </x-table-cell>
+                        <x-table-cell>
+                            {{ $user->email }}
+                        </x-table-cell>
+                        <x-table-cell>
+                            {{ DateFormatter::formatDate($user->birth_date) }}
+                        </x-table-cell>
+                        <x-table-cell>
+                            {{ $user->role }}
+                        </x-table-cell>
+                        <x-table-cell>
+                            {{ DateFormatter::formatTimestamp($user->created_at) }}
+                        </x-table-cell>
+                        <x-table-cell>
+                            <a href="{{route('users.show', $user->id)}}" class="text-blue-600 font-semibold hover:underline">Edit</a>
+                        </x-table-cell>
+                    </x-table-row>
+                @endforeach
+            </x-table-body>
+        </x-table>
+    </div>
 </x-sidebar>
