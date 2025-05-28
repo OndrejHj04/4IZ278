734511a2b035c09105b55b0b4afffa6e4f7fb934
Ondřej Hájek <ondrej.hajek.profi@gmail.com>
2025-05-28 16:03:27 +0200

    veci na ui

--- resources/views/auth/components/login-form.blade.php
+++ resources/views/auth/components/login-form.blade.php
@@ -8,33 +8,13 @@
         <form class="space-y-6" action="{{ route('login') }}" method="POST">
             @csrf
 
-            <div>
-                <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
-                <div class="mt-2">
-                    <input type="email" name="email" id="email" autocomplete="email" required
-                        value="{{ old('email') }}"
-                        class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                </div>
-            </div>
-
-            <div>
-                <div class="flex items-center justify-between">
-                    <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-                </div>
-                <div class="mt-2">
-                    <input type="password" name="password" id="password" autocomplete="current-password" required
-                        class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                </div>
-            </div>
-
-            <div>
-                <button type="submit"
-                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                    in</button>
-            </div>
+            <x-input type="email" name="email">Email address</x-input>
+            <x-input type="password" name="password">Password</x-input>
+            <x-button type="submit">Sign in</x-button>
             @if ($errors->has('credentials'))
                 <p class="text-center text-red-500 font-semibold">You have provided wrong credentials!</p>
             @endif
+            
         </form>
 
         <p class="mt-10 text-center text-sm/6 text-gray-500">
---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,12 @@
+<div >
+    <div class="flex items-center justify-between">
+        <label for="{{ $attributes->get('name') }}" class="block text-sm/6 font-medium text-gray-900">{{ $slot }}</label>
+    </div>
+    <div class="mt-2">
+        <input {{ $attributes->merge(['id' => 'password_confirmation']) }} autocomplete="current-password" required value="{{ old($attributes->get('name')) }}"
+            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+    </div>
+    @error($attributes->get('name'))
+        <p class="text-center text-red-500 font-semibold text-sm">{{ $message }}</p>
+    @enderror
+</div>
